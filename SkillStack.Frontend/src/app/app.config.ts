d1266dec202c6afa12931278b4ffde93401ecb46
valves <valves@WKSJUN000112>
2025-03-17 11:54:12 -0300

    feat: add login

--- SkillStack.Frontend/src/app/app.config.ts
+++ SkillStack.Frontend/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
+import { AuthService } from '../app/services/auth/auth.service';
 import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+import { LoginComponent } from '../app/auth/login/login.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+export const appConfig = {
+  providers: [
+    provideHttpClient(), 
+    provideRouter(routes), 
+    provideAnimations(),
+    AuthService, 
+  ],
+  declarations: [
+    LoginComponent, 
+  ]
 };
--- SkillStack.Frontend/src/app/features/home/home.component.ts
+++ SkillStack.Frontend/src/app/features/home/home.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule], // Importa os módulos do Angular Material
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule], 
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  
+  constructor(private authService: AuthService, private router: Router) {}
 
-  logout() {
-    console.log('Logout');
-    this.router.navigate(['/login']); // Redireciona para a página de login
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        localStorage.removeItem('token');
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.error('Erro ao fazer logout', error);
+      }
+    });
   }
 }
